55a5860579192afd66b2743b38e140aae155341f
gi13090 <barbosagiovannibarbosa@gmail.com>
2026-06-05 16:32:09 -0300

    Atualizando para o render

--- src/main/java/infra/DataBaseConfigs.java
+++ src/main/java/infra/DataBaseConfigs.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 public class DataBaseConfigs {
 
     private static final String URL =
-            "";
+            System.getenv("DB_URL");
 
     private static final String USER =
-            "";
+            System.getenv("DB_USER");
 
     private static final String PASSWORD =
-            "";
+            System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
